d2cb2f800d7de3bb0e6f655f3dbb6ebf6e6a3449
You Song Hou <ysongweb3@gmail.com>
2025-02-11 21:25:40 -0500

    chore: install web3, python-dotenv, eth-account and initialize the contract

--- filframe_ai/src/filframe_ai/main.py
+++ filframe_ai/src/filframe_ai/main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 import sys
 import warnings
+from web3 import Web3
+from eth_account import Account
+from dotenv import load_dotenv
+import os
 
 from datetime import datetime
 
@@ -63,3 +67,240 @@ def test():
 
     except Exception as e:
         raise Exception(f"An error occurred while testing the crew: {e}")
+
+class SmartContractAgent:
+    def __init__(self, contract_address, contract_abi, network_url):
+        # Connect to Ethereum network
+        self.w3 = Web3(Web3.HTTPProvider(network_url))
+        
+        # Load contract
+        self.contract_address = Web3.to_checksum_address(contract_address)
+        self.contract = self.w3.eth.contract(address=self.contract_address, abi=contract_abi)
+        
+        # Load environment variables
+        load_dotenv()
+        self.private_key = os.getenv('PRIVATE_KEY')
+        self.account = Account.from_key(self.private_key)
+        
+        # Initialize state
+        self.last_block = self.w3.eth.block_number
+        self.gas_price_threshold = 50  # in gwei
+
+# Example usage
+def main():
+    CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+    CONTRACT_ABI = """[
+        {
+        "inputs": [
+            {
+            "internalType": "address",
+            "name": "newAIAgent",
+            "type": "address"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+        },
+        {
+        "inputs": [],
+        "name": "DataMarketplace__NotAIAgent",
+        "type": "error"
+        },
+        {
+        "anonymous": false,
+        "inputs": [
+            {
+            "indexed": true,
+            "internalType": "uint32",
+            "name": "dataIndex",
+            "type": "uint32"
+            },
+            {
+            "components": [
+                {
+                "internalType": "string",
+                "name": "description",
+                "type": "string"
+                },
+                {
+                "internalType": "address",
+                "name": "user",
+                "type": "address"
+                }
+            ],
+            "indexed": false,
+            "internalType": "struct DataMarketplace.Data",
+            "name": "data",
+            "type": "tuple"
+            },
+            {
+            "indexed": false,
+            "internalType": "address",
+            "name": "aiAgent",
+            "type": "address"
+            },
+            {
+            "indexed": false,
+            "internalType": "bool",
+            "name": "isValid",
+            "type": "bool"
+            }
+        ],
+        "name": "AIAgentResponded",
+        "type": "event"
+        },
+        {
+        "anonymous": false,
+        "inputs": [
+            {
+            "indexed": true,
+            "internalType": "uint32",
+            "name": "dataIndex",
+            "type": "uint32"
+            },
+            {
+            "components": [
+                {
+                "internalType": "string",
+                "name": "description",
+                "type": "string"
+                },
+                {
+                "internalType": "address",
+                "name": "user",
+                "type": "address"
+                }
+            ],
+            "indexed": false,
+            "internalType": "struct DataMarketplace.Data",
+            "name": "data",
+            "type": "tuple"
+            }
+        ],
+        "name": "NewDataCreated",
+        "type": "event"
+        },
+        {
+        "inputs": [],
+        "name": "aiAgent",
+        "outputs": [
+            {
+            "internalType": "address",
+            "name": "",
+            "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+        },
+        {
+        "inputs": [
+            {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+            }
+        ],
+        "name": "aiTrainingData",
+        "outputs": [
+            {
+            "internalType": "string",
+            "name": "description",
+            "type": "string"
+            },
+            {
+            "internalType": "address",
+            "name": "user",
+            "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+        },
+        {
+        "inputs": [
+            {
+            "internalType": "string",
+            "name": "description",
+            "type": "string"
+            }
+        ],
+        "name": "createNewData",
+        "outputs": [
+            {
+            "components": [
+                {
+                "internalType": "string",
+                "name": "description",
+                "type": "string"
+                },
+                {
+                "internalType": "address",
+                "name": "user",
+                "type": "address"
+                }
+            ],
+            "internalType": "struct DataMarketplace.Data",
+            "name": "",
+            "type": "tuple"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+        },
+        {
+        "inputs": [],
+        "name": "latestDataNum",
+        "outputs": [
+            {
+            "internalType": "uint32",
+            "name": "",
+            "type": "uint32"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+        },
+        {
+        "inputs": [
+            {
+            "components": [
+                {
+                "internalType": "string",
+                "name": "description",
+                "type": "string"
+                },
+                {
+                "internalType": "address",
+                "name": "user",
+                "type": "address"
+                }
+            ],
+            "internalType": "struct DataMarketplace.Data",
+            "name": "data",
+            "type": "tuple"
+            },
+            {
+            "internalType": "uint32",
+            "name": "referenceDataIndex",
+            "type": "uint32"
+            },
+            {
+            "internalType": "bool",
+            "name": "isValid",
+            "type": "bool"
+            }
+        ],
+        "name": "respondToNewData",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+        }
+    ]"""
+
+    NETWORK_URL = "http://127.0.0.1:8545"
+    
+    newContract = SmartContractAgent(CONTRACT_ADDRESS, CONTRACT_ABI, NETWORK_URL)
+
+if __name__ == "__main__":
+    main()
--- hardhat/ignition/modules/DataMarketplace.js
+++ hardhat/ignition/modules/DataMarketplace.js
@@ -5,7 +5,7 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 
 module.exports = buildModule("DataMarketplaceModule", (m) => {
-  const dataMarketplace = m.contract("DataMarketplace", []);
+  const dataMarketplace = m.contract("DataMarketplace", ["0x70997970C51812dc3A010C7d01b50e0d17dc79C8"]);
 
   return { dataMarketplace };
 });
