2328e718a73f4ed24dd0de5dbd8c8bb9e446935b
You Song Hou <ysongweb3@gmail.com>
2025-02-13 00:32:59 -0500

    feat: add function to call createNewData contract method

--- reactjs/src/pages/UploadData.jsx
+++ reactjs/src/pages/UploadData.jsx
@@ -6,15 +6,24 @@ import DataMarketplaceContract from '../artifacts/contracts/DataMarketplace.sol/
 const UploadData = () => {
   const [ethAddress, setETHAddress] = useState('');
   const [userSigner, setUserSigner] = useState(null);
-  
+
   const connectWallet = async () => {
     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
     setETHAddress(accounts[0]);
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     setUserSigner(signer);
-    const DataMarketplace = new ethers.Contract("0x5FbDB2315678afecb367f032d93F642f64180aa3", DataMarketplaceContract.abi, userSigner);
-    console.log(DataMarketplace);
+  }
+
+  const addData = async () => {
+    try {
+      const DataMarketplace = new ethers.Contract("0x5FbDB2315678afecb367f032d93F642f64180aa3", DataMarketplaceContract.abi, userSigner);
+      const transaction = await DataMarketplace.createNewData("Test");
+      const tx = await transaction.wait();
+      console.log(tx);
+    } catch (error) {
+      console.error(error.message);
+    }
   }
 
   return (
@@ -24,6 +33,12 @@ const UploadData = () => {
         className="w-full py-2 px-4 bg-blue-600 mb-4 cursor-pointer text-white rounded-md hover:bg-blue-700 transition-colors focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:bg-blue-400 disabled:cursor-not-allowed flex items-center justify-center"
       >
         {ethAddress ? ethAddress.slice(0, 5) + "..." + ethAddress.slice(37, 42) : 'Connect Wallet'}
+      </button>
+      <button
+        onClick={addData}
+        className="w-full py-2 px-4 bg-blue-600 mb-4 cursor-pointer text-white rounded-md hover:bg-blue-700 transition-colors focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:bg-blue-400 disabled:cursor-not-allowed flex items-center justify-center"
+      >
+       Add data
       </button>
     </div>
   );
